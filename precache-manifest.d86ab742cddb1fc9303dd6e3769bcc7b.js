5ddf2bc32078ebc3f3d7b3dae59de9bba9b37c45
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-09-10 00:34:28 +0300

    Updates

---
+++ precache-manifest.d86ab742cddb1fc9303dd6e3769bcc7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f411480ba605102e5a9cdd2cefce4fe7",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "123da65aebe2114a8cd8",
+    "url": "/react_static-list-of-todos/static/css/main.ba398e71.chunk.css"
+  },
+  {
+    "revision": "fa67027c564020748778",
+    "url": "/react_static-list-of-todos/static/js/2.78963138.chunk.js"
+  },
+  {
+    "revision": "123da65aebe2114a8cd8",
+    "url": "/react_static-list-of-todos/static/js/main.e1f35d41.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
